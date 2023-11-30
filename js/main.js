a23f4da3131336dd4c05c5faad81330912e75921
belenviton <belenviton@hotmail.com>
2023-11-30 21:57:02 +0100

    event description

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@ const form = document.querySelector ('.js-form')
 const cancelBtn = document.querySelector ('.js-btn-cancel')
 const searchBtn = document.querySelector ('.js_btn_search')
 const descripInput = document.querySelector ('.js_in_search_desc')
+
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const inputBreed = document.querySelector('.js-input-breed');
+const labelMessageError = document.querySelector('.js-label-error');
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const button_search = document.querySelector('.js_btn_search');
 /* Agregar el código del li desde HTMl 
 Repetir este proceso por cada gatito */
 const kittenOne = `<li class="card">
@@ -58,15 +66,18 @@ showButton.addEventListener ('click' , (event) => {
    formSection.classList.toggle ('collapsed');
 });
 
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-const inputBreed = document.querySelector('.js-input-breed');
-const labelMessageError = document.querySelector('.js-label-error');
+
 
 /* Código HTML de un gatito */
-
-
+const kittenDesc1 = `Porte elegante, su patrón de color tan característico y sus ojos
+de un azul intenso, pero su historia se remonta a Asía al menos
+hace 500 años, donde tuvo su origen muy posiblemente.`
+const kittenDesc2 = ` Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+hasta con pinta de alienígena han llegado a definir a esta raza
+gatuna que se caracteriza por la «ausencia» de pelo.`
+const kittenDesc3 = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+bella mirada se ha convertido en una de sus señas de identidad.
+Sus ojos son grandes y las orejas resultan largas y en punta.`
 
 addButton.addEventListener ('click' , (event) => {
   event.preventDefault();
@@ -97,3 +108,23 @@ cancelBtn.addEventListener ('click' , (event) => {
   form.reset ();
   formSection.classList.add ('collapsed');
  });
+
+ button_search.addEventListener('click', (event) => {
+  event.preventDefault ();
+  listHTML.innerHTML = '';
+
+  const descrSearchText = input_search_desc.value;
+
+  if( kittenDesc1.includes(descrSearchText) ) {
+    listHTML.innerHTML += kittenOne;
+    }
+    
+    if( kittenDesc2.includes(descrSearchText) ) {
+      listHTML.innerHTML += kittenTwo;
+    }
+    
+    if( kittenDesc3.includes(descrSearchText) ) {
+      listHTML.innerHTML += kittenThree;
+  
+    }
+});
